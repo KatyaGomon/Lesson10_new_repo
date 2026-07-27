1f8ee2c3125d2c6df43eb598c0f0e609675f3658
Katya <kotagomon@gmail.com>
2026-07-27 15:46:44 -0700

    first test

---
+++ test_button.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+
+
+@pytest.fixture()
+def driver():
+    driver = webdriver.Firefox()
+    return driver
+
+
+def test_button_exist(driver):
+    driver.get("https://www.qa-practice.com/elements/button/simple")
+    assert driver.find_element(By.ID, "submit-id-submit").is_displayed()
