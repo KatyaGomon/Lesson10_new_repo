fe34175122848e063b220b471031cc0766e3d98d
Katya <kotagomon@gmail.com>
2026-07-28 16:02:16 -0700

    workflow dispatch

--- test_button.py
+++ test_button.py
@@ -16,3 +16,7 @@ def test_button_exist(driver):
     driver.get("https://www.qa-practice.com/elements/button/simple")
     assert driver.find_element(By.ID, "submit-id-submit").is_displayed()
 
+
+def test_button_exist_2(driver):
+    driver.get("https://www.qa-practice.com/elements/button/like_a_button")
+    assert driver.find_element(By.PARTIAL_LINK_TEXT, 'Click')
