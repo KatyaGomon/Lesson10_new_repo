e38df3516debb12fa9b459d3746545e387885106
Katya <kotagomon@gmail.com>
2026-07-28 15:34:56 -0700

    Five action

--- test_button.py
+++ test_button.py
@@ -8,7 +8,7 @@ from selenium.webdriver.firefox.options import Options
 def driver():
     options = Options()
     options.add_argument('--headless')
-    driver = webdriver.Firefox(options=options)
+    driver = webdriver.Chrome(options=options)
     return driver
 
 
